2fac30f915c3788dac62c54edbdb945cf359e273
novatransmedia <41652611+iasarmientoj@users.noreply.github.com>
2024-11-22 11:11:20 -0500

    poner meta

--- Plataforma-2.5D-2017/Assets/Scripts/Meta.cs
+++ Plataforma-2.5D-2017/Assets/Scripts/Meta.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Meta : MonoBehaviour {
-
+public class Meta : MonoBehaviour
+{
+    public GameObject trofeo; // El GameObject que se mostrará al detener el juego
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Verifica si el player entra en el trigger
         if (collision.CompareTag("Player"))
         {
+            // Pausa el juego
+            Time.timeScale = 0;
 
+            // Muestra el GameObject
+            if (trofeo != null)
+            {
+                trofeo.SetActive(true);
+            }
         }
     }
 }
