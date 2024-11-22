7e05fc355d02b08a57060aa24f16c82d19039064
novatransmedia <41652611+iasarmientoj@users.noreply.github.com>
2024-11-22 02:27:42 -0500

    poner reaparicion

---
+++ Plataforma-2.5D-2017/Assets/Scripts/Reintentar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Reintentar : MonoBehaviour {
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+}
